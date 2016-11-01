f70815569945baa8a8d3cef332cc2f23d3046f50
Peerapat Sungkasem <gigapixel7@gmail.com>
2016-11-01 17:34:57 +0700

    test update file

--- src/routers/test.js
+++ src/routers/test.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', test);
 
 async function test(req, res) {
-  res.json({data: "It's work."});
+  res.json({data: "It's work 555."});
 }
 
 export default router;
