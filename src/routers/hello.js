debbc5d4e5e8dd00ca2da694443e9eabd889e7e7
Peerapat Sungkasem <gigapixel7@gmail.com>
2018-05-28 17:43:11 +0700

    update code

--- src/routers/hello.js
+++ src/routers/hello.js
@@ -4,5 +4,5 @@ export const router = new Router();
 router.get('/', hello);
 
 async function hello(req, res) {
-  res.json({ message: 'hello express' });
+  res.json({ message: 'hello express test' });
 }
