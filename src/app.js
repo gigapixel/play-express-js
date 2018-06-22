2913422d08e47453de6dbec2a3efd9f1471d34aa
Peerapat Sungkasem <gigapixel7@gmail.com>
2018-06-22 20:30:33 +0700

    add pkginfo

--- src/app.js
+++ src/app.js
@@ -1,33 +1,37 @@
 import 'babel-polyfill';
 import express from 'express';
-import bugsnag from 'bugsnag';
+// import bugsnag from 'bugsnag';
+import pkginfo from 'pkginfo';
 
-import { router as hello } from './routers/hello';
-
-bugsnag.register('6ccbcf070657d980b68e35cdaaaf1772', { 
-  logLevel: 'info',
-  releaseStage: 'localhost'
-});
+// bugsnag.register('6ccbcf070657d980b68e35cdaaaf1772', { 
+//   logLevel: 'info',
+//   releaseStage: 'localhost'
+// });
 
 const app = express();
-app.use(bugsnag.requestHandler)
+// app.use(bugsnag.requestHandler)
 
-app.use('/api/hello', (req, res, next) => {
-  bugsnag.notify('hello event', { 
-    data: { 
-      id: 1, 
-      status: 'created',
-      items: [{ sku: '1112' }, { sku: '1150' } ],
-    },
-  });
-  res.status(200).json({ message: 'hello' });
+app.get('/api/version', (req, res, next) => {
+  pkginfo(module, 'name', 'version');
+  res.status(200).json(module.exports);
 });
+
+// app.get('/api/hello', (req, res, next) => {
+//   bugsnag.notify('hello event', { 
+//     data: { 
+//       id: 1, 
+//       status: 'created',
+//       items: [{ sku: '1112' }, { sku: '1150' } ],
+//     },
+//   });
+//   res.status(200).json({ message: 'hello' });
+// });
 
 app.use('/api/error', (req, res, next) => {
   next(new Error('Test custom error'));
 });
 
-app.use(bugsnag.errorHandler)
+// app.use(bugsnag.errorHandler)
 app.use((err, req, res, next) => {
   res.status(400).json({ message: err.message });
 });
