0d76a46f9c24eecfa195749b2c8aa7b42d9389f1
Peerapat Sungkasem <gigapixel7@gmail.com>
2018-07-12 00:03:02 +0700

    update code

--- src/routers/hello.js
+++ src/routers/hello.js
@@ -4,5 +4,5 @@ export const router = new Router();
 router.get('/', hello);
 
 async function hello(req, res, next) {
-  res.status(200).json({ message: 'hello express on k8s' });
+  res.status(200).json({ message: 'hello express test tag push' });
 }
